d9793e18b6a2251da1bdb496303612a20a73c683
Devin Leamy <devinleamy@gmail.com>
2022-10-11 22:36:32 -0400

    completions

---
+++ lua/jot/completions.lua
@@ -0,0 +1,82 @@
+local util = require "jot.util"
+
+local source = {}
+
+-- Initializes a new nvim-cmp source, with 
+-- a the given state
+source.new = function(state)
+  source.state = state
+  return setmetatable({}, { __index = source })
+end
+
+source.get_trigger_characters = function() 
+  return { "[" }
+end
+
+-- nvim-cmp hook; runs whenever the a trigger character 
+-- (defined above) is input.
+source.complete = function(self, request, callback)
+  -- Retrieves the link that is being typed, as a string. 
+  -- Returns nil in no valid link start is found
+  -- 
+  --
+  -- eg: "[[li" for some line "insert [[li"
+  --                                  |||| 
+  --                 link being typed ^^^^      
+  local function get_link_segment(input)
+    for i = string.len(input), 1, -1 do
+      local substr = string.sub(input, i)
+      if vim.endswith(substr, "]") then
+        return nil
+      elseif vim.startswith(substr, "[[") then
+        return substr
+      end
+    end
+    return nil
+  end
+
+  local link_segment = get_link_segment(request.context.cursor_before_line)
+
+  if link_segment  == nil or not vim.startswith(link_segment, "[[") then
+    return callback({})
+  end
+
+  print("Input: " .. link_segment) 
+
+  print(self.state.directories[1]) 
+  local notes = util.collect_notes(self.state.directories)
+
+  local completions = {}
+
+  for i = 1, #notes do
+    local note = notes[i]
+
+    table.insert(completions, {
+      label = note.file_name,
+      -- Behavior when completion is accepted
+      --
+      -- Inserts link "[[<link text>]]" and 
+      -- places cursor after the link
+      textEdit = {
+        newText = "[[" .. note.file_name .. "]]",
+        insert = {
+          start = {
+            line = request.context.cursor.row - 1,
+            -- column of the first "[" in the link
+            character = request.context.cursor.col - 1 - #link_segment,
+          },
+          ['end'] = {
+            line = request.context.cursor.row - 1,
+            -- column after the last "]" in the link
+            character = request.context.cursor.col + 1,
+          },
+        },
+      }
+    })
+  end
+
+
+  return callback(completions)
+end
+
+return source
--- lua/jot/init.lua
+++ lua/jot/init.lua
@@ -1,4 +1,5 @@
 local Path = require "plenary.path"
+local Completions = require "jot.completions"
 
 local jot = {}
 jot.VERSION = "0.1"
@@ -14,18 +15,60 @@ function State:new(options)
     "/Users/Devin/.local/obsidian/dl/neovim",
   }
 
+  new_state.display_completions = true
+  if options.display_completions == false then
+    new_state.display_completions = false 
+  end
+
   self.__index = self
   return setmetatable(new_state, self)
 end
 
+-- Setup jot completions 
+-- TODO: Make this lazyly load when a Markdown buffer 
+-- is created
+jot.setup_completions = function(state)
+  -- nvim_cmp plugin
+  local has_cmp, cmp = pcall(require, "cmp")
+
+  -- Does not have cmp or does not want completions
+  if not has_cmp or not state.display_completions then
+    return
+  end
+
+  cmp.register_source('jot', Completions.new(state))
+
+
+  -- Add cmp_jot.lua to the list of sources
+  local sources = {
+    { name = "jot" }
+  }
+
+  for _, source in pairs(cmp.get_config().sources) do
+    if source.name ~= "jot" then
+      table.insert(sources, source)
+    end
+  end
+
+  cmp.setup.buffer { sources = sources }
+end
+
 -- Set configuration options
+--
+-- @param options jot.Config
 jot.setup = function(options)
   print("Setup jot")
 
   local state = State:new(options)
 
   require("jot.command").register_all(state)
+
+  jot.setup_completions(state)
 end
+
+-- jot.Config
+-- @field directories Array<string> | absolute path of note directories 
+-- @field display_completions boolean | enable completions
 
 return jot
 
